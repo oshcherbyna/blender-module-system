493520c545e44ce0447ebabf42fb41b5192a8da7
oshcherbyna <shcherbyna.o.m@gmail.com>
2024-05-08 22:05:01 +0300

    Add files via upload

---
+++ 0_main_(player_wasd_moving).py
@@ -0,0 +1,28 @@
+'''
+Enrty point.
+'''
+import bpy
+
+__name__ = '[main.py]'
+print(__name__)
+
+module_player = bpy.data.texts["module_player.py"].as_module()
+module_keyconfig = bpy.data.texts["module_keyconfig.py"].as_module()
+
+
+scene = bpy.context.scene
+#player = module_player.init(scene.objects['Cube']) #scene.objects['Camera']) # remove parent and ? dooble run main.py 
+player = module_player.init(scene.objects['Rocket Launcher'])
+scene["player"] = player
+
+module_keyconfig.init() # <= need scene["player"]
+
+
+def game_loop(self, context):
+    module_player.update(player)
+      
+bpy.app.handlers.frame_change_pre.clear()
+bpy.app.handlers.frame_change_pre.append(game_loop) # ON-OFF game_loop
+
+
+
